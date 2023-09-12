b4f9b42f6053ff010dd13855fdfb7c55dd31991e
EC2 Default User <yuzugaia@users.noreply.github.com>
2023-09-12 06:30:00 +0000

    [finish]4-10

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,22 +1,22 @@
 class HomesController < ApplicationController
   def top
-    
+
   end
-  
+
   def about
-    
+
   end
-  
+
   def new
-    
+
   end
-  
+
   def index
-    
+
   end
-  
+
   def show
-    
+
   end
-  
+
 end
