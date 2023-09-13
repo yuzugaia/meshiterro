6af83c68575837ec8835a5581352d0bee6c1901a
EC2 Default User <yuzugaia@users.noreply.github.com>
2023-09-13 05:50:26 +0000

    [finish]4-18

--- db/migrate/20230911105415_create_post_images.rb
+++ db/migrate/20230911105415_create_post_images.rb
@@ -5,6 +5,8 @@ class CreatePostImages < ActiveRecord::Migration[6.1]
       t.string :shop_name
       t.text :caption
       t.integer :user_id
+      t.text :comment
+      t.integer :post_image_id
       t.timestamps
     end
   end
