c60139132085737d1bca50d0ebf4fb01605618ba
EC2 Default User <yuzugaia@users.noreply.github.com>
2023-09-13 04:50:03 +0000

    [finish]4-17

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,11 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(@user.id)  
+    redirect_to user_path(@user.id)
   end
 
 
