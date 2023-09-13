1658cc899862e76f43b87879fd5d2ef705c7be82
EC2 Default User <yuzugaia@users.noreply.github.com>
2023-09-13 04:08:30 +0000

    [finish]4-16

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get 'homes/about' => "homes#about", as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 end
