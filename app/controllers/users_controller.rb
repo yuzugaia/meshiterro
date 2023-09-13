edf491890f13b0ce65f7f8debea6292082ddf5af
EC2 Default User <yuzugaia@users.noreply.github.com>
2023-09-13 07:46:13 +0000

    [finish]4-22

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @post_images = @user.post_images
+    @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
